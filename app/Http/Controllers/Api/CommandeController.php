63fdd0aff9d32aa08a84e2edc01a550736fc6473
Razi Fertani <razifertani@innoscape.com>
2023-01-05 08:17:34 +0100

    master

--- app/Http/Controllers/Api/CommandeController.php
+++ app/Http/Controllers/Api/CommandeController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Commande;
 use App\Models\Establishment;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
 
 class CommandeController extends Controller
 {
@@ -73,61 +71,55 @@ class CommandeController extends Controller
         return view('boutique.commande.show', compact('boutique', 'commande', 'historiqueCMD'));
     }
 
-    public function updateStatus($id, $commandeId, Request $request)
+    public function updateStatus($id, $commandeId)
     {
-        $boutique = Establishment::findOrFail($id);
-        if ($boutique->company_id != Auth::user()->company_id) {
-            abort(404);
+        try {
+            $boutique = Establishment::findOrFail($id);
+            // if ($boutique->company_id != Auth::user()->company_id) {
+            //     abort(404);
+            // }
+
+            $commande = Commande::with('establishment', 'particulier')->findOrFail($commandeId);
+            $commande->update([
+                'status' => request('status'),
+            ]);
+            // $user = User::findOrFail();
+            // $data['id'] = $commande->particulier_id;
+
+            // $data['title'] = 'Votre commande a été mise à jour';
+            // switch ($request->status) {
+            //     case 2:
+            //         $objet = "Votre commande a été annulée ";
+            //         $data['body'] = "Votre commande " . $commandeId . " a été annulée pour le motif suivant : \" " . $commande->message . " \"";
+            //         break;
+            //     case 3:
+            //         $objet = "Votre commande  est en cours de livraison";
+            //         $data['body'] = "Ta commande N°" . $commandeId . " est  confirmée  auprès de " . $commande->establishment->name . ". Elle sera prête a etre récupérer a \"" . $commande->comming_hour . "\".";
+            //         break;
+            //     case 4:
+            //         $objet = "Votre commande  est en cours de livraison";
+            //         $data['body'] = "Votre commande " . $commandeId . " est en cours de livraison. Elle vous sera livrée á \" " . $request->comming_hour . " \"";
+            //         break;
+            //     case 1:
+            //         $objet = "Votre commande  a été livrée";
+            //         $data['body'] = "Votre commande " . $commandeId . " a été livrée, vous pouvez laisser un avis";
+            //         break;
+            //     default:
+            //         # code...
+            //         break;
+            // }
+            return response()->json([
+                'error' => false,
+                'message' => 'Commande mise à jour succès !',
+            ], 200);
+
+        } catch (\Throwable$th) {
+            report($th);
+            return response()->json([
+                'error' => true,
+                'message' => $th->getMessage(),
+            ], 500);
         }
 
-        session(['establishmentId' => $boutique->id]);
-
-        $commande = Commande::with('establishment', 'particulier')->findOrFail($commandeId);
-        $commande->update([
-            'message' => $request->message,
-            // 'comming_hour' => $request->comming_hour,
-            'status' => $request->status,
-        ]);
-        // $user = User::findOrFail();
-        $data['id'] = $commande->particulier_id;
-
-        $data['title'] = 'Votre commande a été mise à jour';
-        switch ($request->status) {
-            case 2:
-                $objet = "Votre commande a été annulée ";
-                $data['body'] = "Votre commande " . $commandeId . " a été annulée pour le motif suivant : \" " . $commande->message . " \"";
-                break;
-            case 3:
-                $objet = "Votre commande  est en cours de livraison";
-                $data['body'] = "Ta commande N°" . $commandeId . " est  confirmée  auprès de " . $commande->establishment->name . ". Elle sera prête a etre récupérer a \"" . $commande->comming_hour . "\".";
-                break;
-            case 4:
-                $objet = "Votre commande  est en cours de livraison";
-                $data['body'] = "Votre commande " . $commandeId . " est en cours de livraison. Elle vous sera livrée á \" " . $request->comming_hour . " \"";
-                break;
-            case 1:
-                $objet = "Votre commande  a été livrée";
-                $data['body'] = "Votre commande " . $commandeId . " a été livrée, vous pouvez laisser un avis";
-                break;
-            default:
-                # code...
-                break;
-        }
-
-        $data['type'] = "Commande_update_status";
-        $data['object_id'] = $commande->id;
-        $to_email = $commande->particulier->email;
-        $to_name = $commande->particulier->name;
-        $data['nom'] = $to_name;
-
-        Services::notifyUser($data);
-
-        Mail::send('emails.cmdstatus', $data, function ($message) use ($to_email, $to_name, $objet) {
-
-            $message->from('Co-cuisinage@outlook.fr', 'Commande statut');
-            $message->to($to_email, $to_name);
-            $message->subject($objet);
-        });
-        return redirect()->route('commande.show', ['id' => $id, 'commandeId' => $commandeId])->with(['success' => 'modifé avec succés']);
     }
 }
--- app/Http/Controllers/Api/ProfessionalController.php
+++ app/Http/Controllers/Api/ProfessionalController.php
@@ -23,7 +23,7 @@ class ProfessionalController extends Controller
                 'company.establishments.roles',
                 'company.establishments.reservations',
                 'company.establishments.schedules',
-                'company.establishments.commandes.commandeProduct',
+                'company.establishments.commandes.commandeProduct.establishmentProduct',
 
                 'plannings.tasks',
                 'company.establishments.professionals.plannings.tasks',
